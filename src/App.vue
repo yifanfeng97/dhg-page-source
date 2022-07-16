ba3fd71b4af921f669661f2f49726305f0928c29
yifanfeng97 <evanfeng97@qq.com>
2022-07-16 12:17:28 +0800

    add app bar

--- src/App.vue
+++ src/App.vue
@@ -5,49 +5,60 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
+      <!-- <template v-slot:img="{ props }">
         <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+          v-bind="props"
+          gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template> -->
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="./assets/logo.jpg"
+        transition="scale-transition"
+        width="40"
+      />
+
+      
+      <!-- <v-app-bar-title>Deep HyperGraph</v-app-bar-title> -->
 
       <v-spacer></v-spacer>
-      
+
+      <v-tabs align-with-title>
+        <v-tab>News</v-tab>
+        <v-tab>Tutorials</v-tab>
+        <v-tab>Docs</v-tab>
+        <v-tab>Github</v-tab>
+      </v-tabs>
+
+      <!-- <v-spacer></v-spacer> -->
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
       <v-btn
+        icon
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <HelloWorld style="height: 2000px" />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     HelloWorld,
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,17 +2,17 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-        <v-img
+        <!-- <v-img
           :src="require('../assets/logo.svg')"
           class="my-3"
           contain
           height="200"
-        />
+        /> -->
       </v-col>
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          Welcome to Deep HyperGraph
         </h1>
 
         <p class="subheading font-weight-regular">
