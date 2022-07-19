4eccc6115f370fb7620cc7264cd51aa32fbea595
yifanfeng97 <evanfeng97@qq.com>
2022-07-19 19:13:41 +0800

    add advantage cards

--- src/components/Advantage.vue
+++ src/components/Advantage.vue
@@ -1,25 +1,22 @@
 <template>
   <v-container>
     <v-row>
-      <v-col class="mb-4 text-h3 text-center" cols="12">
-          Our Advantages
-      </v-col>
+      <v-col class="mb-4 text-h3 text-center" cols="12"> Our Advantages </v-col>
     </v-row>
     <v-row>
-      <v-col cols="12" md="4">
-        <v-card>
-          
+      <v-col cols="12" lg="3" v-for="item in advs" :key="item.idx">
+        <v-card class="text-center">
+          <v-card-text>
+            <span class="mb-5 text-h5 black--text" style="word-break: Keep-all">
+              {{ item.title }}
+            </span>
+          </v-card-text>
+          <v-card-text>
+            <p class="text-body-1">
+              {{ item.content }}
+            </p>
+          </v-card-text>
         </v-card>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo_DHG.svg')"
-          class="my-3"
-          contain
-          max-height="200"
-        />
       </v-col>
     </v-row>
   </v-container>
@@ -32,46 +29,48 @@ export default {
   data: () => ({
     advs: [
       {
-        "idx": 1,
-        "title": "Support High-Order Message Passing",
-        "content": "The details"
+        idx: 1,
+        title: "Support High-Order Message Passing on Structure",
+        content: "The details",
       },
       {
-        "idx": 2,
-        "title": "Share Ecosystem with Pytorch",
-        "content": "The details of the DHG ecosystem are shared with Pytorch. This allows for the use of Pytorch in the DHG ecosystem."
+        idx: 2,
+        title: "Shared Ecosystem with Pytorch",
+        content:
+          "The details of the DHG ecosystem are shared with Pytorch. This allows for the use of Pytorch in the DHG ecosystem.",
       },
       {
-        "idx": 3,
-        "title": "Powerful API for desiging GNNs and HGNNs",
-        "content": "The details"
+        idx: 3,
+        title: "Powerful API for desiging GNNs and HGNNs",
+        content: "The details",
       },
       {
-        "idx": 4,
-        "title": "Structure Visualization of Graphs and Hypergraphs",
-        "content": "The details"
+        idx: 4,
+        title: "Structure Visualization of Graphs and Hypergraphs",
+        content: "The details",
       },
       {
-        "idx": 5,
-        "title": "Bridge the Gap between Graphs and Hypergraphs",
-        "content": "The details"
+        idx: 5,
+        title: "Bridge the Gap between Graphs and Hypergraphs",
+        content: "The details",
       },
       {
-        "idx": 6,
-        "title": "Split the Structure and Trainable Parameters",
-        "content": "The details"
+        idx: 6,
+        title: "Split the Structure and Trainable Parameters",
+        content: "The details",
       },
       {
-        "idx": 7,
-        "title": "Comprehensive and Flexible",
-        "content": "Existing Datasets and Models. Random Graph/Hypergraph Generator"
+        idx: 7,
+        title: "Comprehensive and Flexible",
+        content:
+          "Existing Datasets and Models. Random Graph/Hypergraph Generator",
       },
       {
-        "idx": 8,
-        "title": "Auto-ML Support",
-        "content": "The details"
-      }
-    ]
+        idx: 8,
+        title: "Auto-ML Support",
+        content: "The details",
+      },
+    ],
   }),
 };
 </script>
