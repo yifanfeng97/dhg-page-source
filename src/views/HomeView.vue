0a0e760c5fcb6448543c913beedea2f95eb48d46
Yifan Feng(丰一帆) <evanfeng97@gmail.com>
2022-09-19 21:36:42 +0800

    add video poster

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <video loop muted autoplay playsinline width="100%" :src="video_src">
+    <video
+      loop
+      muted
+      autoplay
+      playsinline
+      width="100%"
+      :src="video_src"
+      :poster="video_poster"
+    >
       <p>Your browser does not support the video tag.</p>
     </video>
     <v-container>
@@ -40,6 +48,22 @@ export default {
           return require("@/assets/neuron_actions_4s_v1_wide_text.mp4");
         default:
           return require("@/assets/neuron_actions_4s_v1_wide_text.mp4");
+      }
+    },
+    video_poster() {
+      switch (this.$vuetify.breakpoint.name) {
+        case "xs":
+          return require("@/assets/neuron_actions_4s_v1_text_poster.jpg");
+        case "sm":
+          return require("@/assets/neuron_actions_4s_v1_text_poster.jpg");
+        case "md":
+          return require("@/assets/neuron_actions_4s_v1_wide_text_poster.jpg");
+        case "lg":
+          return require("@/assets/neuron_actions_4s_v1_wide_text_poster.jpg");
+        case "xl":
+          return require("@/assets/neuron_actions_4s_v1_wide_text_poster.jpg");
+        default:
+          return require("@/assets/neuron_actions_4s_v1_wide_text_poster.jpg");
       }
     },
   },
